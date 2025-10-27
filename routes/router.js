52bcd6e59c3d86dfe6f0e36cbab2b4b032d3d682
Natasha M. Evelyn <natasha.evelyn@mscoding.org>
2025-10-27 13:59:07 -0500

    updating code
    added a 404, home and error page.

--- routes/router.js
+++ routes/router.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const router = express.Router()
 
-//!HOMEPAGE http://localhost:3001 
+//!HOMEPAGE http://localhost:3001 => home page
 router.get('/', (req, res) => {
-    res.send('working...')
+    // res.send('working...') => dispaly home page
+    res.render('pages/home', {
+        title: "Crazpicc's Funny Jokes",
+        name: "crazpicc's jokes app!"
+    })
 })
 
 router.use('/jokes', require('./api/jokesRoutes'))
@@ -11,7 +15,11 @@ router.use('/jokes', require('./api/jokesRoutes'))
 //? error handling (* means anything that isnt in the path would return an error)
 //! Needs to go a the bottom of the page 
 router.use((req, res, next)=> {
-    res.status(404).send("404 Error. This page does not exist!")
+    res.status(404)
+    .render('pages/404', {
+        title: '404 ERROR',
+        name: '404 ERROR. THAT PAGE DOES NOT EXIST'
+    })
 })
 
 module.exports = router
